b1b791bf305882d8b83cc96016a0f43cf88e5401
Ilya <savchenko0501@mail.ru>
2023-04-10 04:18:35 +0800

    Example GET request

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
 <template>
-	<div>
-		Likes: {{ likes }}
-	</div>
-	<button @click="addLike">Like!</button>
+    <div class="app">
+
+    <button @click="fetchData">GET</button>
+    <ul>
+        <li v-for="item in data" :key="item.id">
+            {{ item }}
+        </li>
+    </ul>
+
+    </div>
 </template>
 
 <script>
 export default {
-	data() {
-		return {
-			likes: 4,
-			}
-	},	
+    data() {
+        return {
+            data: [],
+        }
+    },  
 
-	methods: {
-		addLike(){
-			this.likes++;
-		}
-	}
+    methods: {
+        fetchData() {
+            fetch("https://jsonplaceholder.typicode.com/todos")
+                .then(response => response.json())
+                .then(data => {this.data = data;})
+                .catch(error => {console.error("Error, ", error);})
+        },
+    }
 }
 </script>
 
 <style>
+body {
+    background-color: #181818;
+}
+.app {
+    background-color: #222;
+    color: #eee;
+    }
+button {
+    background-color: #474;
+    color: #eee;
+    border: solid #353;
+}
+button:hover {
+    background-color: #585;
+}
 </style>
 
